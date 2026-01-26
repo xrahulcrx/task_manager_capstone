12a77d9e90dc1cbf73ab6c9ca00736f7d225e0f7
unknown <rahulcr2797@gmail.com>
2026-01-26 15:40:35 +0530

    docker script for wsl

--- infra/start.sh
+++ infra/start.sh
@@ -21,6 +21,6 @@ EOF
 
 sudo apt update -y
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Docker installed successfully."
