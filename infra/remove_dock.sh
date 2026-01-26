a4a3a7d3dd882a6f4f50805ae6ebce8c39a84a29
unknown <rahulcr2797@gmail.com>
2026-01-26 15:40:52 +0530

    remove docker script

---
+++ infra/remove_dock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+sudo rm /etc/apt/sources.list.d/docker.sources
+sudo rm /etc/apt/keyrings/docker.asc
